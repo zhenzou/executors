53b376c8585685592db90cf25f659bafe8eb1de7
september <zzzhen1994@gmail.com>
2022-04-23 10:35:15 +0800

    fix(test): schedule test

--- schedule_executor_test.go
+++ schedule_executor_test.go
@@ -2,45 +2,43 @@ package executors
 
 import (
 	"context"
+	"sync/atomic"
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPoolScheduleExecutor_ScheduleAtFixRate(t *testing.T) {
 
 	scheduleExecutor := NewPoolScheduleExecutor(WithMaxConcurrent(10))
 
-	ch1 := make(chan struct{})
-	ch2 := make(chan struct{})
+	var i int64 = 10
+	_, _ = scheduleExecutor.ScheduleAtFixRate(RunnableFunc(func(ctx context.Context) {
+		atomic.AddInt64(&i, 10)
+	},
+	), 100*time.Millisecond)
 
-	t.Run("schedule", func(t *testing.T) {
-		go func() {
-			var i = 10
-			_, _ = scheduleExecutor.Schedule(RunnableFunc(func(ctx context.Context) { i += 10 }), 500*time.Millisecond)
-			time.AfterFunc(1*time.Second, func() {
-				require.Equal(t, 20, i)
-			})
-			println(i)
-			ch1 <- struct{}{}
-		}()
+	time.AfterFunc(1*time.Second, func() {
+		assert.Greater(t, atomic.LoadInt64(&i), 100)
 	})
 
-	t.Run("fix rate", func(t *testing.T) {
-		go func() {
-			var i = 10
-			_, _ = scheduleExecutor.ScheduleAtFixRate(RunnableFunc(func(ctx context.Context) { i += 10 }), 100*time.Millisecond)
+	_ = scheduleExecutor.Shutdown(context.Background())
+}
 
-			time.AfterFunc(1*time.Second, func() {
-				require.Equal(t, 100, i)
-			})
-			println(i)
-			ch2 <- struct{}{}
-		}()
+func TestPoolScheduleExecutor_Schedule(t *testing.T) {
+
+	scheduleExecutor := NewPoolScheduleExecutor(WithMaxConcurrent(10))
+
+	var i int64 = 10
+	_, _ = scheduleExecutor.Schedule(RunnableFunc(func(ctx context.Context) {
+		atomic.AddInt64(&i, 10)
+	},
+	), 500*time.Millisecond)
+
+	time.AfterFunc(1*time.Second, func() {
+		assert.Equal(t, 20, atomic.LoadInt64(&i))
 	})
 
-	<-ch1
-	<-ch2
 	_ = scheduleExecutor.Shutdown(context.Background())
 }
