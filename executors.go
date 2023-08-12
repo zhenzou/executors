69d184f2bc298b8a97f28a52f643ef9351ffbc25
september <zzzhen1994@gmail.com>
2023-08-12 16:47:32 +0800

    doc: add some comments

--- executors.go
+++ executors.go
@@ -43,17 +43,28 @@ func (c CallableFunc[T]) Call(ctx context.Context) (T, error) {
 }
 
 type Executor interface {
+	// Execute execute a task in background
+	// Will return ErrClosed if shutdown already
+	// Will return ErrRejectedExecution if task out of cap
 	Execute(Runnable) error
+
+	// Shutdown shutdown the executor
+	// Will wait the queued task to be finish
 	Shutdown(ctx context.Context) error
 }
 
 type ExecutorService[T any] interface {
 	Executor
+
+	// Submit execute a task with result async, and can get the task result via get
 	Submit(callable Callable[T]) (Future[T], error)
 }
 
 type ScheduledExecutor interface {
 	Executor
+	// Schedule run a one time task after delay duration
 	Schedule(r Runnable, delay time.Duration) (CancelFunc, error)
+
+	// ScheduleAtFixRate schedule a periodic task in fixed rate
 	ScheduleAtFixRate(r Runnable, period time.Duration) (CancelFunc, error)
 }
